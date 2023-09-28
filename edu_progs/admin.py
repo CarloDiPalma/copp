7f1f681ec2f15150d016ffcc02dccaff204746c2
Pavel <ping.06@mail.ru>
2023-09-28 10:08:11 +0300

    Add Program model

--- edu_progs/admin.py
+++ edu_progs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Programm
+
+
+@admin.register(Programm)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'is_free')
+    list_editable = ('is_free',)
+    search_fields = ('name',)
+    list_filter = ('edu_start',)
+    empty_value_display = '-пусто-'
--- edu_progs/models.py
+++ edu_progs/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Programm(models.Model):
+    """Учебная программа."""
+
+    name = models.CharField(max_length=140)
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Описание группы',
+        help_text='Описание программы'
+    )
+    reg_start = models.DateTimeField(
+        verbose_name='дата начала регистрации',
+    )
+    reg_end = models.DateTimeField(
+        verbose_name='дата конца регистрации',
+    )
+    edu_start = models.DateTimeField(
+        verbose_name='дата начала обучения',
+    )
+    edu_end = models.DateTimeField(
+        verbose_name='дата конца обучения',
+    )
+    is_intramural = models.BooleanField(
+        verbose_name='флаг очной формы',
+        default=True
+    )
+    is_free = models.BooleanField()
+    text = models.TextField(
+        verbose_name='подробное описание программы',
+    )
+
+    class Meta:
+        verbose_name = 'Учебная программа'
+        verbose_name_plural = 'Учебные программы'
+
+    def __str__(self):
+        return self.name
