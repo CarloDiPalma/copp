c25352506836717479c76fa91894ccd87f040240
Pavel <ping.06@mail.ru>
2023-10-16 11:12:32 +0300

    Add news to index page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from edu_progs.models import Program
+from news.models import Post
+
 
 def index(request):
-    context = {}
+    progs = Program.objects.filter().order_by('-id')[:3][::-1]
+    posts = Post.objects.filter().order_by('-id')[:3][::-1]
+
+    context = {
+        'progs': progs,
+        'posts': posts
+    }
     return render(request, 'main/index.html', context)
