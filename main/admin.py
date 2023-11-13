2f0f055677d25611936bcf1efb635c1a8c087c7c
Pavel <ping.06@mail.ru>
2023-11-13 13:49:44 +0300

    Add people count to index page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import People
+
+
+@admin.register(People)
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ('count', )
--- main/views.py
+++ main/views.py
@@ -3,16 +3,18 @@ from django.shortcuts import render
 from edu_progs.models import Program
 from events.models import Event
 from news.models import Post
+from .models import People
 
 
 def index(request):
     progs = Program.objects.filter().order_by('-id')[:3][::-1]
     posts = Post.objects.filter().order_by('-id')[:3][::-1]
     events = Event.objects.filter().order_by('-id')[:3][::-1]
-
+    people = People.objects.get(id=1).count
     context = {
         'progs': progs,
         'posts': posts,
-        'events': events
+        'events': events,
+        'people': people
     }
     return render(request, 'main/index.html', context)
