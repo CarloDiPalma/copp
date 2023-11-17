467c78934d48322ef298680a545968e2567d6e7a
Pavel <ping.06@mail.ru>
2023-11-17 09:20:44 +0300

    Add success page after order

--- edu_progs/admin.py
+++ edu_progs/admin.py
@@ -5,16 +5,16 @@ from .models import Program, Order
 
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'edu_start', 'is_free')
+    list_display = ('pk', 'name', 'is_free')
     # list_editable = ('is_free',)
     search_fields = ('name',)
-    list_filter = ('edu_start',)
+    list_filter = ('reg_start', )
     empty_value_display = '-пусто-'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'phone', 'email', 'created', 'program', 'is_handled')
+    list_display = ('pk', 'name', 'phone', 'email', 'created', 'program', 'is_handled')
     readonly_fields = ['created', 'phone', 'email', 'name', 'program']
     search_fields = ('email',)
     list_filter = ('created',)
