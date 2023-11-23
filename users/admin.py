962d49929fb3eb182c070f7bdbdabbb67c2a44e0
Pavel <ping.06@mail.ru>
2023-11-23 09:45:48 +0300

    Add permissions in User admin

--- users/admin.py
+++ users/admin.py
@@ -11,5 +11,16 @@ class CustomUserAdmin(UserAdmin):
 class UserAdmin(CustomUserAdmin):
     list_display = ('pk', 'email', 'is_active', 'last_name')
     search_fields = ('last_name', 'email')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_admin', 'is_staff', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
     # list_filter = ('date',)
     empty_value_display = '-пусто-'
