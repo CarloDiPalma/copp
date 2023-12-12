13aa4702027792ab367f629bd2647bc6be282771
Pavel <ping.06@mail.ru>
2023-12-12 13:52:38 +0300

    Add ckeditor to progs and event admin

--- events/admin.py
+++ events/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django import forms
+from ckeditor.widgets import CKEditorWidget
 
 from .models import Event
 
 
+class EventAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+    text = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    form = EventAdminForm
     list_display = ('pk', 'name', 'date', 'is_published')
     search_fields = ('name',)
     list_filter = ('date',)
