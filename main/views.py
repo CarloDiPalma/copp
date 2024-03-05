ec6fa541807128374b8399bf12358e1e51b2d5ea
Pavel <ping.06@mail.ru>
2024-03-05 09:45:45 +0300

    Add post_number constant

--- main/views.py
+++ main/views.py
@@ -5,11 +5,13 @@ from events.models import Event
 from news.models import Post
 from .models import People
 
+POSTS_NUMBER = 4
+
 
 def index(request):
-    progs = Program.objects.filter().order_by('-id')[:4][::-1]
-    posts = Post.objects.filter().order_by('-id')[:4][::-1]
-    events = Event.objects.filter().order_by('-id')[:4][::-1]
+    progs = Program.objects.filter().order_by('-id')[:POSTS_NUMBER][::-1]
+    posts = Post.objects.filter().order_by('-id')[:POSTS_NUMBER][::-1]
+    events = Event.objects.filter().order_by('-id')[:POSTS_NUMBER][::-1]
     try:
         people = People.objects.get(id=1).count
     except:
