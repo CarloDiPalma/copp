2d24f7ab58cafa67ca558cff746be7f175283981
Pavel <ping.06@mail.ru>
2024-03-11 10:21:01 +0300

    Add admin to vacancy

--- vacancy/admin.py
+++ vacancy/admin.py
@@ -1,3 +1,24 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin import TabularInline
+from ckeditor.widgets import CKEditorWidget
 
-# Register your models here.
+from vacancy.models import Vacancy
+
+
+class VacancyAdminForm(forms.ModelForm):
+    short_story = forms.CharField(widget=CKEditorWidget())
+    full_story = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Vacancy
+        fields = '__all__'
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    form = VacancyAdminForm
+    list_display = ('pk', 'title', 'is_published', 'date')
+    search_fields = ('title',)
+    list_filter = ('date',)
+    empty_value_display = '-пусто-'
