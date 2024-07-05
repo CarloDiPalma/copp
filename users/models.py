604512f685ed8cf38ed2dafdb6de00a6ae17df09
Pavel <ping.06@mail.ru>
2024-07-05 11:56:03 +0300

    Edit create_superuser

--- users/models.py
+++ users/models.py
@@ -62,8 +62,9 @@ class UserManager(BaseUserManager):
         user.last_name = last_name
         user.set_password(password)
         user.admin = True
-        user.staff = True
+        user.is_staff = True
         user.active = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
